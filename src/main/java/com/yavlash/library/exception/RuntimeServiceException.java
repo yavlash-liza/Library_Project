bcb3f50fa27b8d5089ef4d14b806633ce4bee7f3
yavlash-liza <yavlashliza@gmail.com>
2022-07-20 23:04:53 +0300

    add custom exceptions

---
+++ src/main/java/com/yavlash/library/exception/RuntimeServiceException.java
@@ -0,0 +1,7 @@
+package com.yavlash.library.exception;
+
+public class RuntimeServiceException extends RuntimeException {
+    public RuntimeServiceException(Throwable cause) {
+        super(cause);
+    }
+}
